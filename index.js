c0ee9f3758693141e4ef771a7b14c2b557752d0d
Andrés Miravete Carvajal <andresmiravetecarvajal@MacBook-Pro-de-Andres.local>
2019-05-12 15:30:49 +0200

    segunda actualizacion

--- index.js
+++ index.js
@@ -11,17 +11,3 @@ function myMove(){
     }
   }
 }
-
-function myMove() {
-  var elem = document.getElementById("myAnimation");
-  var pos = 0;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 900) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.left = pos + 'px';
-    }
-  }
-}
